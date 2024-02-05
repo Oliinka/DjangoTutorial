367625095df4be338ebaa1c76538bd8528d1cea5
Olinka <olga.titzenthaler@gmail.com>
2024-02-05 23:30:34 +0100

    Django Tutorial part 5 - testing

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,15 +6,14 @@ from django.shortcuts import get_object_or_404
 ##nahrany kod tento preda urls polls a to to zas preda urls mysite...OMG
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-
 from django.template import loader
-
 from django.urls import reverse
 
 from .models import Choice, Question
 
 from django.shortcuts import get_object_or_404, render
 from django.views import generic
+from django.utils import timezone
 
 """def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -34,8 +33,13 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[
+            :5
+        ]
 
 
 """def detail(request, question_id):
@@ -45,6 +49,11 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = "polls/detail.html"
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 """def results(request, question_id):
